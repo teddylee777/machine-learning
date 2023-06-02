33975c8a15358be23d767f4cf2099e2deb70b071
Teddy Lee <teddylee777@gmail.com>
2023-06-02 11:27:37 +0900

    Update fonts-nanum.sh

--- 99-Misc/01-Colab/fonts-nanum.sh
+++ 99-Misc/01-Colab/fonts-nanum.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
-!wget http://data.jaen.kr/download?download_path=%2Fdata%2Ffiles%2FmySUNI%2Fdatasets%2FNanumGothic.zip -O NanumGothic.zip
-!rm -rf NanumGothic
-!mkdir NanumGothic
-!unzip NanumGothic.zip -d NanumGothic
-!rm -rf /usr/share/fonts/truetype/nanum
-!mkdir /usr/share/fonts/truetype/nanum
-!yes | cp -r NanumGothic/NanumGothic.ttf /usr/share/fonts/truetype/nanum/NanumGothic.ttf
-!fc-cache -f -v
-!rm -rf NanumGothic
+wget http://data.jaen.kr/download?download_path=%2Fdata%2Ffiles%2FmySUNI%2Fdatasets%2FNanumGothic.zip -O NanumGothic.zip
+rm -rf NanumGothic
+mkdir NanumGothic
+unzip NanumGothic.zip -d NanumGothic
+rm -rf /usr/share/fonts/truetype/nanum
+mkdir /usr/share/fonts/truetype/nanum
+yes | cp -r NanumGothic/NanumGothic.ttf /usr/share/fonts/truetype/nanum/NanumGothic.ttf
+fc-cache -f -v
+rm -rf NanumGothic
